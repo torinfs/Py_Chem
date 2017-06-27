dbcde13cc768ce43d7f55e0a7219c1c9ef2ea895
torinfs <torins@uw.edu>
2017-06-27 16:11:50 -0700

    Fixed UHF with alpha beta Nelec bug / cleaned up code and output for uscf.py

--- uscf.py
+++ uscf.py
@@ -1,6 +1,6 @@
 # Self-Consistent Field Main Script - Unrestricted
 # Torin Stetina
-# June 5th 2017
+# June 27th 2017
 
 import time
 import numpy as np
@@ -26,14 +26,29 @@ def getIntegrals(molecule):
   # T   = Kinetic energy matrix
   # S   = Overlap matrix
   # ERI = Electron-Electron repulsion tensor
-  direc = 'test_systems/' + molecule
 
+  direc = 'test_systems/' + molecule
   Vnn = genfromtxt('./' + direc + '/Vnn.dat',dtype=None)
   Vne = genfromtxt('./' + direc + '/Vne.dat',dtype=None)
   T   = genfromtxt('./' + direc + '/T.dat',dtype=None)
   S   = genfromtxt('./' + direc + '/S.dat',dtype=None)
   ERI = scipy.io.loadmat( direc + '/ERI.mat', squeeze_me=False) 
   return Vnn, Vne, T, S, ERI['ERI']
+
+
+def calcAlphaBeta(Nelec, mult):
+  # Nelec = # of electrons in system
+  # mult  = multiplicity of system
+
+  if Nelec % 2 == 0:
+    Na, Nb = Nelec//2, Nelec//2
+    Na += (mult-1)/2
+    Nb -= (mult-1)/2
+  elif Nelec % 2 != 0:
+    Na, Nb = Nelec//2, Nelec//2
+    Na += (mult-1)/2 + 1
+    Nb -= (mult-1)/2
+  return Na, Nb
 
 
 def deltaP(P,P_old):  
@@ -50,11 +65,13 @@ start_time = time.time()
 #### TEST SYSTEMS #### 
 
 ## Test Molecules
-#mol, Nelec, name, basis = 'H2_STO3G', 2, 'H2', 'STO-3G'
+#mol, Nelec, name, basis, mult = 'H2_STO3G', 2, 'H2', 'STO-3G', 1
 #mol, Nelec, name, basis, mult = 'HeHplus_STO3G', 2, 'HeH+', 'STO-3G', 1
+#mol, Nelec, name, basis, mult = 'CO_STO3G', 14, 'CO', 'STO-3G', 1 
 #mol, Nelec, name, basis, mult = 'H2O_STO3G', 10, 'Water', 'STO-3G', 1
-mol, Nelec, name, basis, mult = 'Li_STO3G', 3, 'Lithium', 'STO-3G', 2
-#mol, Nelec, name, basis, mult = 'O2_STO3G', 16, 'Oxygen', 'STO-3G', 3
+#mol, Nelec, name, basis, mult = 'Methanol_STO3G', 18, 'Methanol', 'STO-3G', 1
+#mol, Nelec, name, basis, mult = 'Li_STO3G', 3, 'Lithium', 'STO-3G', 2
+mol, Nelec, name, basis, mult = 'O2_STO3G', 16, 'Oxygen', 'STO-3G', 3 
 
 ######################
 
@@ -63,19 +80,15 @@ mol, Nelec, name, basis, mult = 'Li_STO3G', 3, 'Lithium', 'STO-3G', 2
 # Get integrals from files
 Vnn, Vne, T, S, ERI = getIntegrals(mol)
 
-# Number of basis functions dim
+# Number of basis functions dim, Nalpha, Nbeta
 dim = len(S)
-Na = Nelec//2 + mult-1 
-Nb = Nelec//2
+Na, Nb = calcAlphaBeta(Nelec, mult)
 
 # Build Core Hamiltonian
 h = T + Vne
 
 # Set up initial Fock with core guess, and density at 0
-#eps_a, C_a = eigh(h)
-#eps_b, C_b = eigh(h)
-#P_a = np.dot(C_a[:,0:Na],np.transpose(C_a[:,0:Na]))
-#P_b = np.dot(C_b[:,0:Nb],np.transpose(C_b[:,0:Nb]))
+eps_h, C_h = eigh(h)
 F_a, F_b = h, h
 P_a, P_b = np.zeros((dim,dim)), np.zeros((dim,dim))
 C_a, C_b = np.zeros((dim,dim)), np.zeros((dim,dim))
@@ -87,12 +100,12 @@ X = np.dot(Y, np.dot(s, Y.T))
 
 # Initialize variables
 delta  = 1.0
-conver = 1.0e-10
+conver = 1.0e-08
 count  = 0
 
 
 # Start main SCF loop
-while delta > conver and count < 20:
+while delta > conver and count < 100:
   count += 1
   EOne, ETwo, E0 = 0, 0, 0
   Vee_a = np.zeros((dim,dim))
@@ -106,15 +119,15 @@ while delta > conver and count < 20:
           Vee_b[m,n] += (P_a[k,l] + P_b[k,l]) * ERI[m,n,l,k] \
                                   - P_b[k,l]  * ERI[m,k,l,n]
        
-      EOne += 0.5 * (P_a[m,n] + P_b[m,n])*h[m,n] 
-      ETwo += 0.5 *( P_a[m,n]*(h[m,n] + Vee_a[m,n]) + P_b[m,n]*(h[m,n] + Vee_b[m,n]))
-      E0 += 0.5 *( (P_a[m,n] + P_b[m,n])*h[m,n] + P_a[m,n]*(h[m,n] + Vee_a[m,n])\
-                                                + P_b[m,n]*(h[m,n] + Vee_b[m,n]))
+      EOne += (P_a[m,n] + P_b[m,n])*h[m,n] 
+      ETwo += 0.5 *( P_a[m,n]*(Vee_a[m,n]) + P_b[m,n]*(Vee_b[m,n]))
+      E0   += 0.5 *((P_a[m,n] + P_b[m,n])*h[m,n] + P_a[m,n]*(h[m,n] + Vee_a[m,n])\
+                                                 + P_b[m,n]*(h[m,n] + Vee_b[m,n]))
   
-  print 'EOne = ', EOne
-  print 'ETwo = ', ETwo
+  #print 'EOne = ', EOne
+  #print 'ETwo = ', ETwo
   E0 += Vnn
-  print 'E0', E0
+  #print 'E0', E0
 
   # Update Fock alpha and beta
   F_a = h + Vee_a
@@ -146,7 +159,7 @@ while delta > conver and count < 20:
 
 
 # Get Spin Expectation Value
-#<S^2> = 1/4 * [(Tr[Pmz*S])^2 + 2*Tr[Pmz*S*Pmz*S]]
+# <S^2> = 1/4 * [(Tr[Pmz*S])^2 + 2*Tr[Pmz*S*Pmz*S]]
 Pmz = P_a - P_b
 spin_expect = 1/4.0 * ((np.trace(np.dot(Pmz,S)))**2 + 2*np.trace(np.dot(Pmz,S).dot(Pmz).dot(S)))
 
@@ -159,17 +172,18 @@ print '~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ '
 print '                   R e s u l t s            \n'
 print 'Molecule: ' + name 
 print 'Basis: ' + basis
+print 'Multiplicity: ' + str(mult)
 print 'E(SCF) = ' + str(E0) + ' a.u.'
+print '<S'+u'\xb2'+ '> =', spin_expect
 print 'SCF iterations: ' + str(count)
 print 'Elapsed time: ' + str(elapsed_time) + ' sec'
 print ''
-print 'Fock Matrix (alpha) = \n' + np.array_str(F_a)
-print 'Fock Matrix (beta) = \n' + np.array_str(F_b)
-print 'Density Matrix (alpha) = \n' + np.array_str(P_a)
-print 'Density Matrix (beta) = \n' + np.array_str(P_b)
+#print 'Fock Matrix (alpha) = \n' + np.array_str(F_a)
+#print 'Fock Matrix (beta) = \n' + np.array_str(F_b)
+#print 'Density Matrix (alpha) = \n' + np.array_str(P_a)
+#print 'Density Matrix (beta) = \n' + np.array_str(P_b)
 print 'Orbital Energies (alpha) = \n' + str(eps_a) + '\n'
 print 'Orbital Energies (beta) = \n' + str(eps_b) + '\n'
-print '<S'+u'\xb2'+ '> =', spin_expect, '\n'
 print '~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ '
 print ''
 
