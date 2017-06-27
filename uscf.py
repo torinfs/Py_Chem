f57e44ccdc7b706e284949c12e7e0959ebf9925f
torinfs <torins@uw.edu>
2017-06-27 11:47:04 -0700

    Added spin expectation value, still debugging UHF for O2

--- uscf.py
+++ uscf.py
@@ -51,28 +51,31 @@ start_time = time.time()
 
 ## Test Molecules
 #mol, Nelec, name, basis = 'H2_STO3G', 2, 'H2', 'STO-3G'
-#mol, Nelec, name, basis, mult = 'HeHplus_STO3G', 2, 'HeH+', 'STO-3G', 0
+#mol, Nelec, name, basis, mult = 'HeHplus_STO3G', 2, 'HeH+', 'STO-3G', 1
 #mol, Nelec, name, basis, mult = 'H2O_STO3G', 10, 'Water', 'STO-3G', 1
-#mol, Nelec, name, basis, mult = 'Li_STO3G', 3, 'Lithium', 'STO-3G', 2
-mol, Nelec, name, basis, mult = 'O2_STO3G', 16, 'Oxygen', 'STO-3G', 3
+mol, Nelec, name, basis, mult = 'Li_STO3G', 3, 'Lithium', 'STO-3G', 2
+#mol, Nelec, name, basis, mult = 'O2_STO3G', 16, 'Oxygen', 'STO-3G', 3
 
 ######################
 
 
-
-# Define number of MOs
-#nMOs = Nelec //2  # RHF
 
 # Get integrals from files
 Vnn, Vne, T, S, ERI = getIntegrals(mol)
 
 # Number of basis functions dim
 dim = len(S)
+Na = Nelec//2 + mult-1 
+Nb = Nelec//2
 
 # Build Core Hamiltonian
 h = T + Vne
 
 # Set up initial Fock with core guess, and density at 0
+#eps_a, C_a = eigh(h)
+#eps_b, C_b = eigh(h)
+#P_a = np.dot(C_a[:,0:Na],np.transpose(C_a[:,0:Na]))
+#P_b = np.dot(C_b[:,0:Nb],np.transpose(C_b[:,0:Nb]))
 F_a, F_b = h, h
 P_a, P_b = np.zeros((dim,dim)), np.zeros((dim,dim))
 C_a, C_b = np.zeros((dim,dim)), np.zeros((dim,dim))
@@ -87,13 +90,11 @@ delta  = 1.0
 conver = 1.0e-10
 count  = 0
 
-Na = Nelec//2 + mult-1 
-Nb = Nelec//2
 
 # Start main SCF loop
-while delta > conver and count < 300:
+while delta > conver and count < 20:
   count += 1
-  E0 = 0
+  EOne, ETwo, E0 = 0, 0, 0
   Vee_a = np.zeros((dim,dim))
   Vee_b = np.zeros((dim,dim))
   for m in range(0,dim):
@@ -104,11 +105,16 @@ while delta > conver and count < 300:
                                   - P_a[k,l]  * ERI[m,k,l,n]
           Vee_b[m,n] += (P_a[k,l] + P_b[k,l]) * ERI[m,n,l,k] \
                                   - P_b[k,l]  * ERI[m,k,l,n]
-      
+       
+      EOne += 0.5 * (P_a[m,n] + P_b[m,n])*h[m,n] 
+      ETwo += 0.5 *( P_a[m,n]*(h[m,n] + Vee_a[m,n]) + P_b[m,n]*(h[m,n] + Vee_b[m,n]))
       E0 += 0.5 *( (P_a[m,n] + P_b[m,n])*h[m,n] + P_a[m,n]*(h[m,n] + Vee_a[m,n])\
                                                 + P_b[m,n]*(h[m,n] + Vee_b[m,n]))
   
+  print 'EOne = ', EOne
+  print 'ETwo = ', ETwo
   E0 += Vnn
+  print 'E0', E0
 
   # Update Fock alpha and beta
   F_a = h + Vee_a
@@ -139,16 +145,10 @@ while delta > conver and count < 300:
 
 
 
-# Get Spin Contamination
-spin2_exact = ((mult-1)/2.0) * ((mult-1)/2.0 + 1)
-spin_overlap = 0.0
-print C_a, '\n'
-print C_b, '\n'
-for i in range(0,Na):
-  for j in range(Na+1,Nelec):
-    spin_overlap += abs(np.inner(C_a[:,i], C_b[:,j-Na+1]))**2
-
-spin2_tot = spin2_exact + Nb - spin_overlap
+# Get Spin Expectation Value
+#<S^2> = 1/4 * [(Tr[Pmz*S])^2 + 2*Tr[Pmz*S*Pmz*S]]
+Pmz = P_a - P_b
+spin_expect = 1/4.0 * ((np.trace(np.dot(Pmz,S)))**2 + 2*np.trace(np.dot(Pmz,S).dot(Pmz).dot(S)))
 
 
 elapsed_time = time.time() - start_time
@@ -169,8 +169,7 @@ print 'Density Matrix (alpha) = \n' + np.array_str(P_a)
 print 'Density Matrix (beta) = \n' + np.array_str(P_b)
 print 'Orbital Energies (alpha) = \n' + str(eps_a) + '\n'
 print 'Orbital Energies (beta) = \n' + str(eps_b) + '\n'
-print spin2_exact, Nb, spin_overlap, '\n'
-print '<S^2>_UHF =', spin2_tot, '\n'
+print '<S'+u'\xb2'+ '> =', spin_expect, '\n'
 print '~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ '
 print ''
 
